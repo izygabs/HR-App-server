02d1a36fb9b1134815d969fa9d8569a4c2b57cc8
izygabs <80975143+izygabs@users.noreply.github.com>
2024-03-06 15:36:04 +0100

    port change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 7000;
 // const mongoose = require("mongoose");
 const { connectToDB } = require("./services/mongodbConnect");
 const routes = require("./routes/allRoutes");
